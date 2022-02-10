ef11029cffde084ee01d150d78f23ec3d046f214
Mark Fosseddy <markfosseddy@gmail.com>
2022-02-10 14:14:29 +0500

    change random choice

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 import os
 
 scripts_folder = os.path.expanduser("~/color-scripts")
-random_script = random.choice(random.choices(os.listdir(scripts_folder), k=5))
+random_script = random.choice(os.listdir(scripts_folder))
 script_path = os.path.join(scripts_folder, random_script)
 
 subprocess.run(script_path)
